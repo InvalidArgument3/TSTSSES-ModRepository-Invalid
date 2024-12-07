5c8569b3d4bb7a00d870d320aa55c41876c9f5a0
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-12-06 23:51:31 -0600

    Revert "Update PersistentFactionObjectives.cs"
    This reverts commit 385ca57c21f3b93815581611cd9efe05d4ea8d76.

--- DeltaVFactionQuestLog/Data/Scripts/PersistentFactionObjectives.cs
+++ DeltaVFactionQuestLog/Data/Scripts/PersistentFactionObjectives.cs
@@ -474,11 +474,6 @@ namespace Invalid.DeltaVQuestLog
             {
                 DisplayQuestLog(factionObjectives[factionId], customTitle, playerId);
             }
-
-            if (duration > 0)
-            {
-                questLogHideTimes[playerId] = DateTime.UtcNow.AddSeconds(duration);
-            }
         }
 
         private void DisplayQuestLog(List<string> objectives, string title, long playerId)
